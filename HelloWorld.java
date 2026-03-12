58b510cec0fcc9c027bc60fa1c5671eb70c2b1d9
riddhimithastalin <riddhimithastalin@gmail.com>
2026-03-12 20:47:08 +0530

    UC3 display name with default World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        String name;
 
-        if(args.length > 0) {
-            System.out.println("Hello " + args[0]);
+        if (args.length > 0) {
+            name = args[0];
         } else {
-            System.out.println("Hello");
+            name = "World";
         }
 
+        System.out.println("Hello " + name);
     }
 }
