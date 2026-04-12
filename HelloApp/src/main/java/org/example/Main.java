cb07efc50e518c283c0e21d71db47831fec92bf5
riddhimithastalin <riddhimithastalin@gmail.com>
2026-04-13 00:45:50 +0530

    UC7 completed

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -1,19 +1,12 @@
+
 public class Main {
     public static void main(String[] args) {
 
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            String result = "Hello ";
-
-            for (String name : args) {
-                result += name + ", ";
-            }
-
-            // remove last ", "
-            result = result.substring(0, result.length() - 2);
-
-            System.out.println(result);
+            String result = String.join(", ", args);
+            System.out.println("Hello " + result);
         }
     }
 }
